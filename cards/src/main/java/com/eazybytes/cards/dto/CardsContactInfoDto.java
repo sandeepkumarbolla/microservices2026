35403b725d81d0dc603825df5ffc2b691820311b
sandeepkumarbolla <sandeepchowdarybolla159@gmail.com>
2026-02-26 21:26:54 +0530

    commit

---
+++ cards/src/main/java/com/eazybytes/cards/dto/CardsContactInfoDto.java
@@ -0,0 +1,13 @@
+package com.eazybytes.cards.dto;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "cards")
+public class CardsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+}
